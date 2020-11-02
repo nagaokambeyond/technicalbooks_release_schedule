71d0a26627fc90297d1d9e118054e53ce48e95f1
nagaokambeyond <nagaokamfx@yahoo.co.jp>
2020-11-03 03:07:47 +0900

    path異常修正

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   outputDir: 'docs',      // npm run buildの出力先
-  publicPath: "./",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? "/technicalbooks_release_schedule/"
+    : "./",
   assetsDir: './',
   filenameHashing: false  // jsファイル名にハッシュをつけない
 }
