64ef10e33603fddd9fa2e1ac3881be5799b69b8f
nagaokambeyond <nagaokamfx@yahoo.co.jp>
2020-11-03 11:04:06 +0900

    path異常修正

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -39,11 +39,11 @@ export default {
   },
   mounted: function() {
     axios
-      .get("/assets/json/computer_book_keywords.json")
+      .get("@/assets/json/computer_book_keywords.json")
       .then(response => (this.book_keywords = response.data))
       .catch(response => console.log(response));
     axios
-      .get("/assets/json/computer_books.json")
+      .get("@/assets/json/computer_books.json")
       .then(response => {
         let processed = response.data;
         processed.forEach(book => {
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   outputDir: 'docs',      // npm run buildの出力先
+  //publicPath: process.env.BASE_URL,
   //publicPath: './',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/technicalbooks_release_schedule/'
     : '/',
+  //assetsDir: process.env.NODE_ENV === 'production'
+  //  ? '/technicalbooks_release_schedule/assets/'
+  //  : './assets/',
+  //assetsDir: '',
   //assetsDir: './assets/',
   filenameHashing: false  // jsファイル名にハッシュをつけない
 }
