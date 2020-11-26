38af7e5adb81abbe990e5c274e98f49cde297cb8
nagaokambeyond <nagaokamfx@yahoo.co.jp>
2020-11-26 23:47:25 +0900

    エラー対応
    [Vue warn]: Error in render: "TypeError: Cannot read property '0' of null"

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -1,8 +1,11 @@
 <template>
   <b-navbar type="light" variant="light">
-    <h3>{{ this.child_active_menu[0].menu_title_head }}</h3>
+    <h3 v-if="this.child_active_menu !== null">
+      {{ this.child_active_menu[0].menu_title_head }}
+    </h3>
   </b-navbar>
 </template>
+
 <script>
 export default {
   name: "Head",
