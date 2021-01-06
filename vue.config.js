5b524d76a2248850b03f73d5d828245dab98c747
nagaokambeyond <nagaokamfx@yahoo.co.jp>
2021-01-06 22:14:17 +0900

    warning対応
    This can impact web performance.

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ module.exports = {
   outputDir: 'docs',      // npm run buildの出力先
   publicPath: process.env.NODE_ENV === 'production'
     ? '/technicalbooks_release_schedule/'
-    : '/'
+    : '/',
+  chainWebpack: config => {
+    config.performance
+      .hints(false)
+  }
   //filenameHashing: false  // jsファイル名にハッシュをつけない
 }
